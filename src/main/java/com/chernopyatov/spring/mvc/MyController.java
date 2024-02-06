f27c5fa16247c579af83c160acbe81664e584105
Chernopyatov <Aleksander.Chernopyatov@magenta-technology.com>
2024-02-06 19:30:00 +0400

    update files

--- src/main/java/com/chernopyatov/spring/mvc/MyController.java
+++ src/main/java/com/chernopyatov/spring/mvc/MyController.java
@@ -2,11 +2,14 @@ package com.chernopyatov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+@RequestMapping("/employee")
 public class MyController {
     @RequestMapping("/")
     public String showFirstView(){
@@ -14,7 +17,8 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmployeeDetails(){
+    public String askEmployeeDetails(Model model){
+        model.addAttribute("employee", new Employee());
         return "ask-emp-details-view";
     }
 //    @RequestMapping("/showDetails")
@@ -22,11 +26,27 @@ public class MyController {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model){
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr." + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        return "show-emp-details-view";
+//    }
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model){
+//        empName = "Mr." + empName + "!";
+//        model.addAttribute("nameAttribute", empName);
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails(HttpServletRequest request, Model model){
-        String empName = request.getParameter("employeeName");
-        empName = "Mr." + empName;
-        model.addAttribute("nameAttribute", empName);
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp){
+        String name = emp.getName();
+        emp.setName("Mr. " + emp.getName());
+        emp.setSurname(emp.getSurname() + "!");
+        emp.setSalary(emp.getSalary()*10);
         return "show-emp-details-view";
     }
 }
--- src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html>
 <html>
 <body>
@@ -5,11 +6,27 @@
 <h2>Dear Employee, Please enter your details</h2>
 <br>
 <br>
-<form action="showDetails" method ="get">
-<input type="text" name="employeeName"
-placeholder="Write your name"/>
-<input type="submit"/>
-</form>
+<form:form action="showDetails" modelAttribute="employee">
+    Name: <form:input path="name"/>
+    <br><br>
+    Surname: <form:input path="surname"/>
+    <br><br>
+    Salary <form:input path="salary"/>
+    <br><br>
+    Department <form:select path="department">
+    <form:options items="${employee.departments}"/>
+    </form:select>
+    <br><br>
+    Which car do you want?
+    <form:radiobuttons path="carBrand" items="${employee.carBrands}"/>
+    <br><br>
+    Foreign language(s):
+    EN <form:checkbox path="languages" value="English"/>
+    DE <form:checkbox path="languages" value="Deutsch"/>
+    FR <form:checkbox path="languages" value="French"/>
+    <br><br>
+    <input type="submit" value="OK">
+</form:form>
 
 </body>
 </html>
